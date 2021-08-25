c4e8ad1b082679b90c10cd119233fcb261673d10
n-howson <89506419+n-howson@users.noreply.github.com>
2021-08-25 16:39:29 +0930

    Create Sidebar Items

--- app/ui.R
+++ app/ui.R
@@ -1,5 +1,20 @@
 dashboardPage(
     dashboardHeader(title = 'Code Test'),
-    dashboardSidebar(),
-    dashboardBody()
+    dashboardSidebar(
+        menuItem('Message Submission', tabName = 'submission', icon = icon('sign-in-alt')),
+        menuItem('Message Table', tabName = 'table', icon = icon('table')),
+        menuItem("Git Repository", icon = icon("github-square"), 
+                 href = "https://github.com/n-howson/code_test")
+    ),
+    dashboardBody(
+        tabItems(
+            tabItem(tabName = "submission",
+                    h2("Message Submission")
+            ),
+            
+            tabItem(tabName = "table",
+                    h2("Message Table")
+            )
+        )
+    )
 )
